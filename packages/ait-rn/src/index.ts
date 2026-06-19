189c8ba503f348a7b537580b48ed68679a6acac0
imjlk <i@m.junglei.kim>
2026-06-19 09:51:52 +0900

    feat: add ait-rn runtime guards

--- packages/ait-rn/src/index.ts
+++ packages/ait-rn/src/index.ts
@@ -2,5 +2,6 @@ export * from "./ads";
 export * from "./haptics";
 export * from "./identity";
 export * from "./login";
+export * from "./runtime";
 export * from "./share";
 export * from "./storage";
